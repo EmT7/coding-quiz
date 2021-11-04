47f47ef2b1a4247453bae98c8c31ef0558f3f04d
Miranda <88161424+MirandaT77@users.noreply.github.com>
2021-11-03 19:45:29 -0700

    added functions for questions and results

--- script.js
+++ script.js
@@ -1,69 +1,92 @@
-var quiz = document.getElementById("startQuiz");
-var startQuizButton = document.getElementById("startButton");
-var quizTimer = document.getElementById("timer");
-
-var questionsEl = document.getElementById("questions");
+var quizBody = document.getElementById("quiz");
 var resultsEl = document.getElementById("result");
-var quizEndedDiv = document.getElementById("quizEnded");
 var finalScoreEl = document.getElementById("finalScore");
+var quizEndedDiv = document.getElementById("quizEnded");
+var questionsEl = document.getElementById("questions");
+var quizTimer = document.getElementById("timer");
+var startQuizButton = document.getElementById("startBtn");
+var startQuizDiv = document.getElementById("startpage");
+var highscoreContainer = document.getElementById("highscoreContainer");
+var highscoreDiv = document.getElementById("high-scorePage");
 var highscoreInputName = document.getElementById("initials");
+var highscoreDisplayName = document.getElementById("highscore-initials");
+var endGameBtns = document.getElementById("endGameBtns");
 var submitScoreBtn = document.getElementById("submitScore");
-
+var highscoreDisplayScore = document.getElementById("highscore-score");
 
 // Prompt Questions
 var buttonA = document.getElementById("a");
 var buttonB = document.getElementById("b");
 var buttonC = document.getElementById("c");
 var buttonD = document.getElementById("d");
-var quiz = [
+
+var quizQuestions = [
     {
     question: "What does CSS stand for?",
-    optionA: "Cascading StyleStudio",
-    optionB: "Cresting StyleSheets",
+    optionA: "Cascading Style Studio",
+    optionB: "Cresting Style Sheets",
     optionC: "Caligraphy Standard Style",
-    optionD: "Cascading StyleSheets",
-    correctAnswer: "D"},
+    optionD: "Cascading Style Sheets",
+    correctAnswer: "d"},
     {
     question: "What is NOT a main language of Web Development?",
     optionA: "Microsoft",
     optionB: "JavaScript",
     optionC: "CSS",
     optionD: "HTML",
-    correctAnswer: "A"},
+    correctAnswer: "a"},
     {
-    question: "",
-    optionA: "",
-    optionB: "",
-    optionC: "",
-    optionD: "",
-    correctAnswer: ""},
+    question: "Can you build a responsive quiz with just HTML & CSS?",
+    optionA: "Yes, always",
+    optionB: "No, never",
+    optionC: "It depends on web builder",
+    optionD: "Only when using HTML & CSS together",
+    correctAnswer: "b"},
     {
-    question: "",
-    optionA: "",
-    optionB: "",
-    optionC: "",
-    optionD: "",
-    correctAnswer: ""},
+    question: "JavaScript is a case senstive language?",
+    optionA: "It depends on how it is used",
+    optionB: "It is not a language",
+    optionC: "True",
+    optionD: "False",
+    correctAnswer: "c" },
     {
-    question: "",
-    optionA: "",
-    optionB: "",
-    optionC: "",
-    optionD: "",
-    correctAnswer: ""},
+    question: "What does HTML stand for?",
+    optionA: "HyperText Marketing Language",
+    optionB: "HighTime Markup Language",
+    optionC: "HeroText Markdown Language",
+    optionD: "None of the above",
+    correctAnswer: "d"},
 ];
 
 // Timer & Score
+var finalQuestionIndex = quizQuestions.length;
+var currentQuestionIndex = 0;
 var timeLeft = 65;
 var timerInterval;
 var score = 0;
 var correct;
-var finalQuestionIndex = quizQuestions.length;
-var currentQuestionIndex = 0;
 
+
+function generateQuizQuestion(){
+    quizEndedDiv.style.display = "none";
+    if (currentQuestionIndex === finalQuestionIndex){
+        return showScore();
+    } 
+    var currentQuestion = quizQuestions[currentQuestionIndex];
+    questionsEl.innerHTML = "<p>" + currentQuestion.question + "</p>";
+    buttonA.innerHTML = currentQuestion.optionA;
+    buttonB.innerHTML = currentQuestion.optionB;
+    buttonC.innerHTML = currentQuestion.optionC;
+    buttonD.innerHTML = currentQuestion.optionD;
+};
+
+// Start Quiz 
 function startQuiz(){
+    quizEndedDiv.style.display = "none";
+    startQuizDiv.style.display = "none";
     generateQuizQuestion();
 
+    //Timer
     timerInterval = setInterval(function() {
         timeLeft--;
         quizTimer.textContent = "Time left: " + timeLeft;
@@ -73,25 +96,23 @@ function startQuiz(){
           showScore();
         }
       }, 1000);
-    quizBody.style.display = "block"; 
-    }
+    quizBody.style.display = "block";
+}
 
-function generateQuizQuestion(){
-    gameoverDiv.style.display = "none";
-    if (currentQuestionIndex === finalQuestionIndex){
-        return showScore();
-    } 
-    var currentQuestion = quizQuestions[currentQuestionIndex];
-    questionsEl.innerHTML = "<p>" + currentQuestion.question + "</p>";
-    buttonA.innerHTML = currentQuestion.choiceA;
-    buttonB.innerHTML = currentQuestion.choiceB;
-    buttonC.innerHTML = currentQuestion.choiceC;
-    buttonD.innerHTML = currentQuestion.choiceD;
-};
+function showScore(){
+    quizBody.style.display = "none"
+    quizEndedDiv.style.display = "flex";
+    clearInterval(timerInterval);
+    highscoreInputName.value = "";
+    finalScoreEl.innerHTML = "You got " + score + " out of " + quizQuestions.length + " correct!";
+}
+
 
 submitScoreBtn.addEventListener("click", function highscore(){
+
+
     if(highscoreInputName.value === "") {
-        alert("Initials cannot be blank");
+        alert("Please Enter Initials (REQUIRED)");
         return false;
     }else{
         var savedHighscores = JSON.parse(localStorage.getItem("savedHighscores")) || [];
@@ -101,7 +122,7 @@ submitScoreBtn.addEventListener("click", function highscore(){
             score : score
         };
 
-        gameoverDiv.style.display = "none";
+        quizEndedDiv.style.display = "none";
         highscoreContainer.style.display = "flex";
         highscoreDiv.style.display = "block";
         endGameBtns.style.display = "flex";
@@ -114,14 +135,49 @@ submitScoreBtn.addEventListener("click", function highscore(){
 
 });
 
+
+function generateHighscores(){
+    highscoreDisplayName.innerHTML = "";
+    highscoreDisplayScore.innerHTML = "";
+    var highscores = JSON.parse(localStorage.getItem("savedHighscores")) || [];
+    for (i=0; i<highscores.length; i++){
+        var newNameSpan = document.createElement("li");
+        var newScoreSpan = document.createElement("li");
+        newNameSpan.textContent = highscores[i].name;
+        newScoreSpan.textContent = highscores[i].score;
+        highscoreDisplayName.appendChild(newNameSpan);
+        highscoreDisplayScore.appendChild(newScoreSpan);
+    }
+}
+
+
+function showHighscore(){
+    startQuizDiv.style.display = "none"
+    quizEndedDiv.style.display = "none";
+    highscoreContainer.style.display = "flex";
+    highscoreDiv.style.display = "block";
+    endGameBtns.style.display = "flex";
+
+    generateHighscores();
+}
+
+
+function clearScore(){
+    window.localStorage.clear();
+    highscoreDisplayName.textContent = "";
+    highscoreDisplayScore.textContent = "";
+}
+
+
 function replayQuiz(){
     highscoreContainer.style.display = "none";
-    gameoverDiv.style.display = "none";
+    quizEndedDiv.style.display = "none";
     startQuizDiv.style.display = "flex";
-    timeLeft = 76;
+    timeLeft = 65;
     score = 0;
     currentQuestionIndex = 0;
 }
+
 
 function checkAnswer(answer){
     correct = quizQuestions[currentQuestionIndex].correctAnswer;
@@ -131,12 +187,12 @@ function checkAnswer(answer){
         alert("That Is Correct!");
         currentQuestionIndex++;
         generateQuizQuestion();
-        //display in the results div that the answer is correct.
+        
     }else if (answer !== correct && currentQuestionIndex !== finalQuestionIndex){
         alert("That Is Incorrect.")
         currentQuestionIndex++;
         generateQuizQuestion();
-        //display in the results div that the answer is wrong.
+        
     }else{
         showScore();
     }
